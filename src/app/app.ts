6366a2ff3c032acf3e759865ed681aa2d055780c
Alyssa Nicoll <mail@alyssanicoll.com>
2026-04-09 15:30:44 -0500

    the end of section 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected readonly title = 'Recipe Box';
+  showMessage = false;
+
+  recipes = [
+    { id: 1, name: 'Tacos', isFavorite: false },
+    { id: 2, name: 'Pasta', isFavorite: false },
+    { id: 3, name: 'Cookies', isFavorite: true }
+  ];
+
+  addRecipe() {
+    this.recipes.push({
+      id: Date.now(),
+      name: 'New Recipe',
+      isFavorite: false
+    });
+  }
+
+  showFavorites = false;
+
+  toggleFavorites() {
+    this.showFavorites = !this.showFavorites;
+  }
 }
